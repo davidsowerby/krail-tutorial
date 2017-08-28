a5ee495b639f3a31e9286ca60eaf6555fa2dab01
David Sowerby <david.sowerby@virgin.net>
2017-08-28 14:35:42 +0100

    UI & Theme complete

--- src/main/java/com/example/tutorial/app/BindingManager.java
+++ src/main/java/com/example/tutorial/app/BindingManager.java
@@ -1,11 +1,13 @@
 package com.example.tutorial.app;
 
+import com.example.tutorial.i18n.LabelKey;
 import com.example.tutorial.pages.AnnotatedPagesModule;
 import com.example.tutorial.pages.MyPages;
 import com.example.tutorial.pages.MyPublicPages;
 import com.google.inject.Module;
 import uk.q3c.krail.core.guice.DefaultBindingManager;
 import uk.q3c.krail.core.navigate.sitemap.SystemAccountManagementPages;
+import uk.q3c.krail.core.ui.DefaultUIModule;
 
 import java.util.List;
 
@@ -27,5 +29,10 @@ public class BindingManager extends DefaultBindingManager {
         baseModules.add(new MyPages().rootURI("private/finance-department"));
         baseModules.add(new AnnotatedPagesModule());
         baseModules.add(new MyPublicPages());
+    }
+
+    @Override
+    protected Module uiModule() {
+        return new DefaultUIModule().uiClass(TutorialUI.class).applicationTitleKey(LabelKey.Krail_Tutorial);
     }
 }
---
+++ src/main/java/com/example/tutorial/app/TutorialUI.java
@@ -0,0 +1,27 @@
+package com.example.tutorial.app;
+
+import com.google.inject.Inject;
+import com.vaadin.annotations.Theme;
+import com.vaadin.data.util.converter.ConverterFactory;
+import com.vaadin.server.ErrorHandler;
+import uk.q3c.krail.core.i18n.I18NProcessor;
+import uk.q3c.krail.core.navigate.Navigator;
+import uk.q3c.krail.core.push.Broadcaster;
+import uk.q3c.krail.core.push.PushMessageRouter;
+import uk.q3c.krail.core.ui.ApplicationTitle;
+import uk.q3c.krail.core.ui.DefaultApplicationUI;
+import uk.q3c.krail.core.user.notify.VaadinNotification;
+import uk.q3c.krail.core.view.component.*;
+import uk.q3c.krail.i18n.CurrentLocale;
+import uk.q3c.krail.i18n.Translate;
+import uk.q3c.krail.option.Option;
+
+@Theme("valo")
+public class TutorialUI extends DefaultApplicationUI {
+
+    @Inject
+    protected TutorialUI(Navigator navigator, ErrorHandler errorHandler, ConverterFactory converterFactory, ApplicationLogo logo, ApplicationHeader header, UserStatusPanel userStatusPanel, UserNavigationMenu menu, UserNavigationTree navTree, Breadcrumb breadcrumb, SubPagePanel subpage, MessageBar messageBar, Broadcaster broadcaster, PushMessageRouter pushMessageRouter, ApplicationTitle applicationTitle, Translate translate, CurrentLocale currentLocale, I18NProcessor translator, LocaleSelector localeSelector, VaadinNotification vaadinNotification, Option option) {
+        super(navigator, errorHandler, converterFactory, logo, header, userStatusPanel, menu, navTree, breadcrumb, subpage, messageBar, broadcaster,
+                pushMessageRouter, applicationTitle, translate, currentLocale, translator, localeSelector, vaadinNotification, option);
+    }
+}
